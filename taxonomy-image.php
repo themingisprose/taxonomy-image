dcfccdaa2f150e09b8b111f42e6ffc9bc7528ffd
RogerTM <roger213tm@gmail.com>
2018-12-19 19:24:24 -0500

    Commit inicial para Taxonomy Image

---
+++ taxonomy-image.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:		Taxonomy Image
+ * Plugin URI:		https://github.com/themingisprose/taxonomy-image
+ * Description:		A simple way to add images to categories, tags and custom taxonomies
+ * Version:			1.0
+ * Author:			Theming is Prose Team
+ * Author URI:		https://themingisprose.com
+ * Text Domain:		taxonomy_image
+ * Domain Path:		/locale/
+ * License:			GPLv2
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.html
+ */
+?>
